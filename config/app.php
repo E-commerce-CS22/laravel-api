13c4b753d567fb4c06c6e9f7e6e84da12886f83a
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2025-01-19 22:41:34 +0300

    add CORS support to the application with configuration and middleware

--- config/app.php
+++ config/app.php
@@ -123,4 +123,14 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'cors' => [
+        'paths' => ['api/*'],
+        'allowed_methods' => ['*'],
+        'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', '*')],
+        'allowed_headers' => ['*'],
+        'exposed_headers' => [],
+        'max_age' => 0,
+        'supports_credentials' => false,
+    ],
+
 ];
