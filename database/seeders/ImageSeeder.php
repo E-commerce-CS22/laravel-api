292a943198cecf0ca38304a666de277bf82d638d
anas-abdulmajeed <153741990+anas-abdulmajeed@users.noreply.github.com>
2025-04-14 17:35:19 +0300

    add image seeder

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('product_images')->insert([
+            [
+                'product_id' => 1,
+                'product_variant_id' => null,
+                'image' => '1.png',
+                'alt_text' => 'Image 1',
+                'is_primary' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 2,
+                'product_variant_id' => null,
+                'image' => '2.webp',
+                'alt_text' => 'Image 2',
+                'is_primary' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 3,
+                'product_variant_id' => null,
+                'image' => '3.webp',
+                'alt_text' => 'Image 3',
+                'is_primary' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 4,
+                'product_variant_id' => null,
+                'image' => '4.webp',
+                'alt_text' => 'Image 4',
+                'is_primary' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'product_id' => 5,
+                'product_variant_id' => null,
+                'image' => '5.webp',
+                'alt_text' => 'Image 3',
+                'is_primary' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
